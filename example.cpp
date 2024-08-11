cd020716c58dbbaa6ce988a6f69975703fde7268
ShawnZhong <github@shawnzhong.com>
2024-08-11 16:36:06 +0000

    Fix CI

--- example.cpp
+++ example.cpp
@@ -4,7 +4,7 @@ void foo() { for (volatile int i = 0; i < 100000000; i++); }
 void bar() { for (volatile int i = 0; i < 100000000; i++); }
 
 int main() {
-  PerfControl perf({"perf", "record"});
+  PerfControl perf({"perf", "record", "-o", "perf.data"});
   foo();
   perf.enable();
   bar();  // Only record this
